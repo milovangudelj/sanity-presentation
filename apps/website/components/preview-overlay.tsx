62d703536b5506cb622fe9391864fcc94e851115
Milovan Gudelj <milovan.gudelj@gmail.com>
2024-06-30 11:00:50 +0200

    Animation ✨

--- apps/website/components/preview-overlay.tsx
+++ apps/website/components/preview-overlay.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState, type ReactNode } from "react";
+import { motion } from "framer-motion";
 
 export function PreviewOverlay({
   children,
@@ -19,11 +20,35 @@ export function PreviewOverlay({
   }, [isDraftMode]);
 
   return visible ? (
-    <div className="absolute inset-0 p-2 bg-[#13141b] flex flex-col gap-2">
-      <div className="flex flex-1 flex-col overflow-hidden rounded-lg bg-zinc-100">
+    <motion.div
+      className="absolute inset-0 bg-[#13141b] flex flex-col gap-2"
+      initial={{
+        padding: "0px",
+      }}
+      animate={{
+        padding: "8px",
+      }}
+    >
+      <motion.div
+        className="flex flex-1 flex-col overflow-hidden bg-zinc-100"
+        initial={{
+          borderRadius: "0px",
+        }}
+        animate={{
+          borderRadius: "8px",
+        }}
+      >
         <div className="flex-1 overflow-y-auto">{children}</div>
-      </div>
-      <div className="flex flex-none select-none items-center justify-between bg-[#13141b] font-geist text-[14px]/[16px] text-white">
+      </motion.div>
+      <motion.div
+        className="flex flex-none select-none overflow-hidden items-center justify-between bg-[#13141b] font-geist text-[14px]/[16px] text-white"
+        initial={{
+          height: "0px",
+        }}
+        animate={{
+          height: "auto",
+        }}
+      >
         <div className="flex items-center">
           <span className="relative flex h-2.5 w-2.5">
             <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75" />
@@ -37,8 +62,8 @@ export function PreviewOverlay({
         >
           <span className="inline-block px-1.5">Exit preview</span>
         </a>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   ) : (
     <>{children}</>
   );
