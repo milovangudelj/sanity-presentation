cfedf610fa3841206a98c4c70e0d9aaa2f8e2755
Milo <49202538+milovangudelj@users.noreply.github.com>
2024-07-02 00:34:07 +0200

    Removed `require-await` rule

--- packages/config-eslint/next.js
+++ packages/config-eslint/next.js
@@ -52,5 +52,6 @@ module.exports = {
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/require-await": "off",
   },
 };
