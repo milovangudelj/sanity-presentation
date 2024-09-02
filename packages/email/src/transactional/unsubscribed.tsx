acc91d74cd231f7d4cc9286d83cf0d50aed381ba
Milovan Gudelj <49202538+milovangudelj@users.noreply.github.com>
2024-09-02 11:07:42 +0200

    Updated temporary transactional templates

--- packages/email/src/transactional/unsubscribed.tsx
+++ packages/email/src/transactional/unsubscribed.tsx
@@ -26,9 +26,14 @@ export function UnsubscribedEmail({ id }: UnsubscribedEmailProps) {
       <Html>
         <Head />
         <Preview>You have unsubscribed from our newsletter!</Preview>
-        <Body className="font-sans bg-transparent text-onyx">
-          <Container className="bg-cream mx-auto pt-8 pb-12 mb-16">
-            <Section className="px-12">
+        <Body className="font-sans bg-cream text-onyx">
+          <Container
+            style={{
+              maxWidth: "600px",
+            }}
+            className="mx-auto pt-8 pb-12 mb-16"
+          >
+            <Section className="px-4">
               <Link href={process.env.NEXT_PUBLIC_WEBSITE_URL}>
                 <Img
                   src={`${process.env.NEXT_PUBLIC_WEBSITE_URL}/images/logo.png`}
@@ -37,6 +42,13 @@ export function UnsubscribedEmail({ id }: UnsubscribedEmailProps) {
                   height="36"
                 />
               </Link>
+              <Hr
+                className="my-8"
+                style={{
+                  borderTop: "1px solid #0c0e06",
+                  opacity: 0.1,
+                }}
+              />
               <Hr className="my-8 border-onyx/40" />
               <Text className="text-[16px]/[1.1] text-onyx text-left">
                 Thank you for the time spent with us!
@@ -45,10 +57,63 @@ export function UnsubscribedEmail({ id }: UnsubscribedEmailProps) {
                 You have unsubscribed from our newsletter. It's sad to see you
                 go, but if you come back we'll welcome you with open arms.
               </Text>
-              <Text className="text-[16px]/[1.1] text-onyx text-left">
-                — The Quill team
-              </Text>
-              <Hr className="my-8 border-onyx/40" />
+              <div
+                style={{
+                  margin: "32px 0 0",
+                  width: "100%",
+                  display: "table",
+                  tableLayout: "fixed",
+                }}
+              >
+                <div
+                  style={{
+                    display: "table-cell",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "inline-block",
+                      verticalAlign: "middle",
+                    }}
+                  >
+                    <Img
+                      src={
+                        "https://cdn.sanity.io/images/2hd6r3o4/production/c6c57dccdc4e25f7e556b046fd02b95702bcc75c-1280x1280.jpg?fit=max&auto=format"
+                      }
+                      alt="Milovan G."
+                      width="48"
+                      height="48"
+                      style={{
+                        borderRadius: "50%",
+                        objectFit: "cover",
+                        objectPosition: "center",
+                        width: "48px",
+                        height: "48px",
+                      }}
+                    />
+                  </div>
+                  <Text
+                    className="text-[16px]/[1.1] text-onyx text-left"
+                    style={{
+                      margin: "0 0 0 16px",
+                      verticalAlign: "middle",
+                      display: "inline-block",
+                    }}
+                  >
+                    Milovan G., The Quill team
+                  </Text>
+                </div>
+              </div>
+              <Hr
+                className="my-8"
+                style={{
+                  borderTop: "1px solid #0c0e06",
+                  opacity: 0.1,
+                }}
+              />
               <Footer id={id} />
             </Section>
           </Container>
--- packages/email/src/transactional/welcome.tsx
+++ packages/email/src/transactional/welcome.tsx
@@ -27,9 +27,14 @@ export function WelcomeEmail({ id, resubscribed = false }: WelcomeEmailProps) {
       <Html>
         <Head />
         <Preview>You have successfully subscribed to our newsletter!</Preview>
-        <Body className="font-sans bg-transparent text-onyx">
-          <Container className="bg-cream mx-auto pt-8 pb-12 mb-16">
-            <Section className="px-12">
+        <Body className="font-sans bg-cream text-onyx">
+          <Container
+            style={{
+              maxWidth: "600px",
+            }}
+            className="mx-auto pt-8 pb-12 mb-16"
+          >
+            <Section className="px-4">
               <Link href={process.env.NEXT_PUBLIC_WEBSITE_URL}>
                 <Img
                   src={`${process.env.NEXT_PUBLIC_WEBSITE_URL}/images/logo.png`}
@@ -38,6 +43,13 @@ export function WelcomeEmail({ id, resubscribed = false }: WelcomeEmailProps) {
                   height="36"
                 />
               </Link>
+              <Hr
+                className="my-8"
+                style={{
+                  borderTop: "1px solid #0c0e06",
+                  opacity: 0.1,
+                }}
+              />
               <Hr className="my-8 border-onyx/40" />
               <Text className="text-[16px]/[1.1] text-onyx text-left">
                 {resubscribed ? "Welcome back!" : "Thank you for subscribing!"}
@@ -46,10 +58,63 @@ export function WelcomeEmail({ id, resubscribed = false }: WelcomeEmailProps) {
                 You have successfully subscribed to our newsletter! You will now
                 receive updates and news about our products and services.
               </Text>
-              <Text className="text-[16px]/[1.1] text-onyx text-left">
-                — The Quill team
-              </Text>
-              <Hr className="my-8 border-onyx/40" />
+              <div
+                style={{
+                  margin: "32px 0 0",
+                  width: "100%",
+                  display: "table",
+                  tableLayout: "fixed",
+                }}
+              >
+                <div
+                  style={{
+                    display: "table-cell",
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "inline-block",
+                      verticalAlign: "middle",
+                    }}
+                  >
+                    <Img
+                      src={
+                        "https://cdn.sanity.io/images/2hd6r3o4/production/c6c57dccdc4e25f7e556b046fd02b95702bcc75c-1280x1280.jpg?fit=max&auto=format"
+                      }
+                      alt="Milovan G."
+                      width="48"
+                      height="48"
+                      style={{
+                        borderRadius: "50%",
+                        objectFit: "cover",
+                        objectPosition: "center",
+                        width: "48px",
+                        height: "48px",
+                      }}
+                    />
+                  </div>
+                  <Text
+                    className="text-[16px]/[1.1] text-onyx text-left"
+                    style={{
+                      margin: "0 0 0 16px",
+                      verticalAlign: "middle",
+                      display: "inline-block",
+                    }}
+                  >
+                    Milovan G., The Quill team
+                  </Text>
+                </div>
+              </div>
+              <Hr
+                className="my-8"
+                style={{
+                  borderTop: "1px solid #0c0e06",
+                  opacity: 0.1,
+                }}
+              />
               <Footer id={id} canUnsubscribe />
             </Section>
           </Container>
